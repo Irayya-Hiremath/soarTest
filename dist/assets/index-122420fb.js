077ea67b6991063b8c52f81bc857b6bad2daf81e
Irayya Hiremath <74768938+Irayya-Hiremath@users.noreply.github.com>
2024-12-31 12:56:49 +0530

    app issues fixed

---
+++ dist/assets/index-122420fb.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-e512f3d5.js";const i=()=>e.jsx("div",{children:"Privileges Page"});export{i as default};
---
+++ dist/assets/index-d2a28e73.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-e512f3d5.js";function s(){return e.jsx("div",{className:"grid",children:"Services"})}export{s as default};
